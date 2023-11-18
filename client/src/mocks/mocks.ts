fc15344aa192b3f31221854bfaefa0ac01ff9803
arthurjamesoncode <arthurj17@gmail.com>
2023-11-18 14:54:54 +0000

    added unit tests for apiService

--- client/src/mocks/mocks.ts
+++ client/src/mocks/mocks.ts
@@ -96,3 +96,87 @@ export const mockGames = [
     createdAt: new Date().toISOString(),
   },
 ];
+
+
+export const mockNewGames = [
+  {
+    player: 'Alice',
+    winner: 1,
+    duration: 45,
+  },
+  {
+    player: 'Bob',
+    winner: 2,
+    duration: 30,
+  },
+  {
+    player: 'Charlie',
+    winner: 1,
+    duration: 50,
+  },
+  {
+    player: 'David',
+    winner: 2,
+    duration: 20,
+  },
+  {
+    player: 'Eva',
+    winner: 1,
+    duration: 35,
+  },
+  {
+    player: 'Frank',
+    winner: 2,
+    duration: 40,
+  },
+  {
+    player: 'Grace',
+    winner: 1,
+    duration: 55,
+  },
+  {
+    player: 'Henry',
+    winner: 2,
+    duration: 25,
+  },
+  {
+    player: 'Ivy',
+    winner: 1,
+    duration: 42,
+  },
+  {
+    player: 'Jack',
+    winner: 2,
+    duration: 28,
+  },
+  {
+    player: 'Kate',
+    winner: 1,
+    duration: 48,
+  },
+  {
+    player: 'Leo',
+    winner: 2,
+    duration: 18,
+  },
+  {
+    player: 'Mia',
+    winner: 1,
+    duration: 37,
+  },
+  {
+    player: 'Noah',
+    winner: 2,
+    duration: 32,
+  },
+  {
+    player: 'Olivia',
+    winner: 1,
+    duration: 53,
+  },
+  {
+    player: 'Peter',
+    winner: 2,
+    duration: 22,
+  },
+]

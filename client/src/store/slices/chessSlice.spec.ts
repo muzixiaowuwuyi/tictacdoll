a8b215473c72dc004fd24e37c94c2b92e9160db9
arthurjamesoncode <arthurj17@gmail.com>
2023-11-18 15:42:54 +0000

    test: added unit test for setIntervalId and fixed unit test for starting the game

--- client/src/store/slices/chessSlice.spec.ts
+++ client/src/store/slices/chessSlice.spec.ts
@@ -12,19 +12,37 @@ describe('chessReducer', () => {
 
   it('should handle starting the game', () => {
     const stateBeforeAction = initialState;
-    const action = {type: 'chess/startGame'}
+    const action = { type: 'chess/startGame' };
 
     const result = chessReducer(stateBeforeAction, action);
 
-    expect(result).toStrictEqual({...initialState, isInGame: true, startTime: Date.now()})
-  })
+    expect(result).toStrictEqual({
+      ...initialState,
+      isInGame: true,
+      startTime: result.startTime,
+    });
+
+    expect(result.startTime).toBeTruthy();
+
+    expect(Date.now() - result.startTime!).toBeLessThan(5);
+  });
 
   it('should handle ending the game', () => {
-    const stateBeforeAction = {...initialState, isInGame: true}
-    const action = {type: 'chess/endGame'}
+    const stateBeforeAction = { ...initialState, isInGame: true };
+    const action = { type: 'chess/endGame' };
 
     const result = chessReducer(stateBeforeAction, action);
 
-    expect(result).toStrictEqual({...initialState, gameEnded: true})
-  })
+    expect(result).toStrictEqual({ ...initialState, gameEnded: true });
+  });
+
+  it('should handle updating intervalId', () => {
+    const stateBeforeAction = initialState;
+    const intervalId = setInterval(() => 'placeholder', 10);
+    const action = { type: 'chess/setIntervalId', payload: { intervalId } };
+
+    const result = chessReducer(stateBeforeAction, action);
+
+    expect(result).toStrictEqual({ ...initialState, intervalId });
+  });
 });

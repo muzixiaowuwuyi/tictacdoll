09b154fac80f2728dd45595a3c5d68788b217fca
Kamil <kzmuda888@gmail.com>
2023-11-21 11:23:27 +0000

    added register page

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,11 +1,13 @@
-import { useState, FormEvent, useEffect } from "react";
+import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import logo1 from "/logos-and-icons/logo-1.png";
 import logo2 from "/logos-and-icons/logo-2.png";
 
 function Login() {
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
 
   function handleChange(event: FormEvent<HTMLInputElement>) {
     if (event.currentTarget.id === "username") {
@@ -15,19 +17,19 @@ function Login() {
     }
   }
 
-  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+  function handleLogin(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     // TODO: login logic, make call to the server
   }
 
   return (
-    <div className="login-page">
-       <div className="img-container">
-          <img className="logo1" src={logo1} alt="img1" />
-          <img className="logo2" src={logo2} alt="img2" />
-        </div>
-      <div className="login-container">
-        <form onSubmit={handleSubmit} className="login-form">
+    <div className="user-page">
+      <div className="img-container">
+        <img className="logo1" src={logo1} alt="img1" />
+        <img className="logo2" src={logo2} alt="img2" />
+      </div>
+      <div className="form-container">
+        <form onSubmit={handleLogin} className="login-form">
           <div className="fields">
             <div className="input-field">
               <label className="input-label">Username:</label>
@@ -55,19 +57,22 @@ function Login() {
             </div>
           </div>
           <div className="button-container">
-            <button type="submit" className="login-button">
+            <button type="submit" className="orange-button">
               Login
             </button>
           </div>
-          <div className="button-container">
-            <button className="register-button">
-              Register
-            </button>
-          </div>
         </form>
+        <div className="button-container">
+          <button
+            className="distinct-button"
+            onClick={() => navigate("/register")}
+          >
+            Register
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Login;
--- client/src/components/Register/Register.tsx
+++ client/src/components/Register/Register.tsx
@@ -1,11 +1,73 @@
-import { useState, FormEvent, useEffect } from "react";
-import './Register.css';
-
+import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleChange(event: FormEvent<HTMLInputElement>) {
+    if (event.currentTarget.id === "username") {
+      setUsername(event.currentTarget.value);
+    } else if (event.currentTarget.id === "password") {
+      setPassword(event.currentTarget.value);
+    }
+  }
+
+  function handleRegister() {
+    // TODO: register logic, send data to the server
+  }
+
   return (
-    <div>
-      <h1>Register Works</h1>
+    <div className="user-page">
+        <div className="img-container">
+          <div className="prompt">
+            <h2>Register your details</h2>
+          </div>
+        </div>
+
+      <div className="form-container">
+        <form onSubmit={handleRegister} className="login-form">
+          <div className="fields">
+            <div className="input-field">
+              <label className="input-label">Username:</label>
+              <input
+                id="username"
+                type="text"
+                placeholder="Enter your username"
+                value={username}
+                onChange={handleChange}
+                className="username-input"
+                // required
+              />
+            </div>
+            <div className="input-field">
+              <label className="input-label">Password:</label>
+              <input
+                id="password"
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={handleChange}
+                className="password-input"
+                // required
+              />
+            </div>
+          </div>
+          <div className="button-container">
+            <button type="submit" className="orange-button" onClick={handleRegister}>
+              Register
+            </button>
+          </div>
+          <div className="button-container">
+            <button className="distinct-button" onClick={() => navigate('/')}>
+              Go back
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }

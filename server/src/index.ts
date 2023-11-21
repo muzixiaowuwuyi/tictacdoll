d5cb5b4562cf183980723a2e1421502a678f432c
arthurjamesoncode <arthurj17@gmail.com>
2023-11-21 11:24:08 +0000

    chore: added environment variables for host, port, DB_NAME and DB_URL

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,12 +6,14 @@ import router from './router'
 
 const app: Express = express();
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3002;
+const HOST: string = process.env.HOST ? process.env.HOST : 'localhost';
+
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`); // eslint-disable-line no-console
+app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`); // eslint-disable-line no-console
 });

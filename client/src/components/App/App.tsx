e9a7beb3469c11f25b37c797564176a3615c6a3c
Kamil <kzmuda888@gmail.com>
2023-11-22 11:37:55 +0000

    refactored popup local states into redux store

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import { useState, Dispatch, SetStateAction } from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import GameCanvas from "../GameCanvas/GameCanvas";
 import Navbar from "../Navbar/Navbar";
@@ -9,34 +8,15 @@ import Login from "../Login/Login";
 import Register from "../Register/Register";
 import Gamemode from "../Gamemode/Gamemode";
 
-export type Props = {
-  showPopUp: boolean;
-  setShowPopup: Dispatch<SetStateAction<boolean>>;
-  popUpMessage: string;
-  setPopUpMessage: (message: string) => void;
-}
-
 function App() {
-
-  const [showPopUp, setShowPopup] = useState(false);
-  const [popUpMessage, setPopUpMessage] = useState("");
 
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Login
-         showPopUp={showPopUp}
-         setShowPopup={setShowPopup}
-         popUpMessage={popUpMessage}
-         setPopUpMessage={setPopUpMessage}
-        />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register"
-        element={<Register
-        showPopUp={showPopUp}
-        setShowPopup={setShowPopup}
-        popUpMessage={popUpMessage}
-        setPopUpMessage={setPopUpMessage}/>} />
+        element={<Register />} />
         <Route path="/gamemode" element={<Gamemode />} />
         <Route path="/local" element={<Local />} />
         <Route
